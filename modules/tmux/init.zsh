1c99e233cd067518045fcded0ef809cf9e47c21c
Stefan Schulze Frielinghaus <stefan@sf-email.de>
2019-06-07 18:24:09 +0200

    Initial setup

--- modules/tmux/init.zsh
+++ modules/tmux/init.zsh
@@ -30,15 +30,16 @@ if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && -z "$VSCOD
   tmux start-server
 
   # Create a 'prezto' session if no session has been defined in tmux.conf.
-  if ! tmux has-session 2> /dev/null; then
-    zstyle -s ':prezto:module:tmux:session' name tmux_session || tmux_session='prezto'
-    tmux \
-      new-session -d -s "$tmux_session" \; \
-      set-option -t "$tmux_session" destroy-unattached off &> /dev/null
-  fi
+  # if ! tmux has-session 2> /dev/null; then
+  #   zstyle -s ':prezto:module:tmux:session' name tmux_session || tmux_session='prezto'
+  #   tmux \
+  #     new-session -d -s "$tmux_session" \; \
+  #     set-option -t "$tmux_session" destroy-unattached off &> /dev/null
+  # fi
 
   # Attach to the 'prezto' session or to the last session used. (detach first)
-  exec tmux $_tmux_iterm_integration attach-session -d
+  # exec tmux $_tmux_iterm_integration attach-session -d
+  exec tmux
 fi
 
 #
